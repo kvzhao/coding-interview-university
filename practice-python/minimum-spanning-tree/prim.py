f688ce96036e61e24e4677be2127d898c2733679
kvzhao <kelispinor@gmail.com>
2021-08-16 10:56:42 +0800

    add prime

--- practice-python/minimum-spanning-tree/prim.py
+++ practice-python/minimum-spanning-tree/prim.py
@@ -1,8 +1,44 @@
 
+import heapq
+from heapq import heapify, heappop, heappush
+from collections import defaultdict
 from typing import List
 
+manhattan = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
 def minCostConnectPoints(points: List[List[int]]) -> int:
-    pass
+    n = len(points)
+    vertices = defaultdict(list)
+    for i in range(n):
+        for j in range(i+1, n):
+            dist = manhattan(points[i], points[j])
+            vertices[i].append((dist, j))
+            vertices[j].append((dist, i))
+
+    visited = [False] * n
+    heap = []
+    # init
+    heap.extend(vertices[0])
+    visited[0] = True
+    heapify(heap)
+
+    res = 0
+    count = 1
+    while heap:
+        cost, j = heappop(heap)
+
+        if not visited[j]:
+            visited[j] = True
+            res += cost
+            count += 1
+
+            for neighbor in vertices[j]:
+                heappush(heap, neighbor)
+
+        if count >= n:
+            break
+
+    return res
 
 points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
 
