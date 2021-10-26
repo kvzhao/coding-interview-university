f7a9fba42bbba0bac7304973999f482e6255253a
kvzhao <kelispinor@gmail.com>
2021-10-26 17:53:41 +0800

    add official sol

--- algo-6.006/2018/Problem Sets/ps1/search_sorted_2D_array.py
+++ algo-6.006/2018/Problem Sets/ps1/search_sorted_2D_array.py
@@ -1,4 +1,4 @@
-def search_sorted_2D_array(A, v):
+def search_sorted_2D_array_v1(A, v):
     '''
     Return tuple (x, y) such that A[y][x] == v, or None.
     Input:  A | Array of equal length arrays of integers 
@@ -21,3 +21,23 @@ def search_sorted_2D_array(A, v):
             else:
                 right = x - 1
     return None
+
+
+def search_sorted_2D_array(A, v):
+
+    def search_subarray(A, v, x0, y0, x1, y1):
+
+        if (x1 < x0) or (y1 < y0):
+            return None
+
+        val = A[y1][x0]
+
+        if val > v:
+            return search_subarray(A, v, x0, y0, x1, y1 - 1)
+        if val < v:
+            return search_subarray(A, v, x0 + 1, y0, x1, y1)
+
+        return (x0, y1)
+
+    # upper-left, bottom-right
+    return search_subarray(A, v, 0, 0, len(A[0]) - 1, len(A) - 1)
