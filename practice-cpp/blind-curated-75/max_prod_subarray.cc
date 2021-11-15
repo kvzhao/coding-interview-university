c64f74979b6e2180f914a2dc005149a9d8c54032
kvzhao <kelispinor@gmail.com>
2021-11-15 17:32:43 +0800

    add a solution i dont know

--- practice-cpp/blind-curated-75/max_prod_subarray.cc
+++ practice-cpp/blind-curated-75/max_prod_subarray.cc
@@ -7,8 +7,21 @@ using namespace std;
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
-        return 0;
+        if (nums.size() == 0) return 0;
+
+        int min_so_far = nums[0];
+        int max_so_far = nums[0];
+        int result = max_so_far;
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            auto current = nums[i];
+            int temp = max(current, max(max_so_far * current, min_so_far * current));
+            min_so_far = min(current, min(max_so_far * current, min_so_far * current));
+            max_so_far = temp;
+            result = max(max_so_far, result);
+        }
+
+        return result;
     }
 };
 
@@ -21,6 +34,12 @@ int main() {
 
     auto ret = sol.maxProduct(nums);
 
+    if (ret == ans) cout << "passed\n";
+    else cout << "failed, " << ret << "\n";
+
+    nums = {0, 2};
+    ans = 2;
+    ret = sol.maxProduct(nums);
     if (ret == ans) cout << "passed\n";
     else cout << "failed, " << ret << "\n";
 
